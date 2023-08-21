b6e37dcefad0cb6194d848b88aa0f49cb0428da4
William Rocha <wiilliamdroocha@gmail.com>
2023-08-21 16:23:33 -0300

    ex 2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+ def ask
+
+ end
+
+def answer
+
+  if params[:question] ==  "I am going to work"
+    @answer = "Great"
+  elsif params[:question].last == "?"
+    @answer = "Silly question, get dressed and go to work!."
+  else params[:question] == " "
+    @answer = "I don't care, get dressed and go to work!"
+
+end
+end
+end
